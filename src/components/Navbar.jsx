8d39bf46ba6d6e77c2731c66d4ac7e97370444e8
Aravindhan127 <aravindhan.r@talentakeaways.com>
2025-03-26 23:19:33 +0530

    first commit easyAdmin

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,33 +1,34 @@
 // Navbar.jsx
-import React, { useState } from 'react';
-import './Navbar.css';
+import React, { useState } from "react";
+import "./Navbar.css";
 import AdminPanel from "./AdminPanel";
-import Withdraw from './withdraw';
+import Withdraw from "./Withdraw";
 
 const Navbar = () => {
-    const [activeTab, setActiveTab] = useState('user');
-console.log(activeTab)
-    return (<>
-        <nav className="navbar">
-            <button 
-                className={activeTab === 'user' ? 'active': ''} 
-                onClick={() => setActiveTab('user')}
-                >
-                User Details
-            </button>
-            <button 
-                className={activeTab === 'withdraw' ? 'active' : ''} 
-                onClick={() => setActiveTab('Withdraw')}
-                >
-                Withdraw Details
-            </button>
-        </nav>
+  const [activeTab, setActiveTab] = useState("user");
+  console.log(activeTab);
+  return (
+    <>
+      <nav className="navbar">
+        <button
+          className={activeTab === "user" ? "active" : ""}
+          onClick={() => setActiveTab("user")}
+        >
+          User Details
+        </button>
+        <button
+          className={activeTab === "withdraw" ? "active" : ""}
+          onClick={() => setActiveTab("Withdraw")}
+        >
+          Withdraw Details
+        </button>
+      </nav>
 
-          <div className='table'>
-          {activeTab === 'Withdraw'? <Withdraw/>: <AdminPanel/>}
-       </div>
-        </>
-    );
+      <div className="table">
+        {activeTab === "Withdraw" ? <Withdraw /> : <AdminPanel />}
+      </div>
+    </>
+  );
 };
 
 export default Navbar;
